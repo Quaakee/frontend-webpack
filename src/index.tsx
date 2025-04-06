fb0b04a3f500e17436cf3d51771a0591588fda0d
Quaakee <ishaan.lahoti@gmail.com>
2025-04-06 04:12:48 -0500

    Built!

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+// Direct polyfill imports
 import 'buffer';
 import 'process';
 
+// Make Buffer global
+window.Buffer = window.Buffer || require('buffer').Buffer;
+
+// Initialize the root element
 const rootElement = document.getElementById('root');
 
 if (!rootElement) {
--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,10 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
+  entry: './src/index.tsx',
   output: {
     path: path.join(__dirname, '/build'),
     filename: 'bundle.js',
@@ -14,9 +15,9 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html',
       favicon: './public/favicon.ico',
-      inject: false,
+      inject: true,
     }),
-    new NodePolyfillPlugin(),
+    // Don't use NodePolyfillPlugin - use explicit polyfills
     new CopyWebpackPlugin({
       patterns: [
         {
@@ -26,6 +27,16 @@ module.exports = {
           }
         }
       ]
+    }),
+    // Explicitly provide Buffer and process
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process',
+    }),
+    // Define global process.env
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env),
+      'process.browser': JSON.stringify(true),
     }),
   ],
   module: {
@@ -54,15 +65,28 @@ module.exports = {
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader',
-        options: { limit: false }
+        type: 'asset/resource',
       }
     ]
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
     fallback: {
-      fs: false
+      fs: false,
+      path: require.resolve('path-browserify'),
+      buffer: require.resolve('buffer/'),
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      zlib: require.resolve('browserify-zlib'),
+      util: require.resolve('util/'),
+      process: require.resolve('process/browser'),
+    },
+    alias: {
+      process: require.resolve('process/browser'),
     }
+  },
+  // Disable optimization to avoid ESM issues
+  optimization: {
+    concatenateModules: false
   }
 }
